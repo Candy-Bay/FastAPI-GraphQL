25cbb738f55531279d80866918b6f7d29e48c1a6
Jacob Sussmilch <suisuss@protonmail.com>
2021-09-10 16:03:54 +1000

    Added CORS

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import graphene
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
-
+from fastapi.middleware.cors import CORSMiddleware
 from schema import Query, Mutation
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation)))
 
